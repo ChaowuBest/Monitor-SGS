3ddb6995f3ee53a99237137832e7b2f4f05a70fa
多啦S梦 <1553992118@qq.com>
2022-06-20 08:41:12 +0800

    Add project files.

---
+++ Monitor-SGS/Program.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Toolkit.Uwp.Notifications;
+using System.Text;
+using System.Threading;
+
+namespace Monitor_SGS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Request req = new Request();
+            string info = "Parm1=%7B%22CompanyNo%22%3A1%2C%22SyndicateNo%22%3A1%2C%22ObjectMainGroupNo%22%3A17%2C%22Advertisements%22%3A%5B%7B%22No%22%3A-1%7D%5D%2C%22RentLimit%22%3A%7B%22Min%22%3A0%2C%22Max%22%3A25000%7D%2C%22AreaLimit%22%3A%7B%22Min%22%3A0%2C%22Max%22%3A150%7D%2C%22ApplySearchFilter%22%3Atrue%2C%22Page%22%3A1%2C%22Take%22%3A10%2C%22SortOrder%22%3A%22SeekAreaDescription%20ASC%2C%20street%20ASC%22%2C%22ReturnParameters%22%3A%5B%22ObjectNo%22%2C%22FirstEstateImageUrl%22%2C%22Street%22%2C%22SeekAreaDescription%22%2C%22PlaceName%22%2C%22ObjectSubDescription%22%2C%22ObjectArea%22%2C%22RentPerMonth%22%2C%22MarketPlaceDescription%22%2C%22CountInterest%22%2C%22FirstInfoTextShort%22%2C%22FirstInfoText%22%2C%22EndPeriodMP%22%2C%22FreeFrom%22%2C%22SeekAreaUrl%22%2C%22Latitude%22%2C%22Longitude%22%2C%22BoardNo%22%5D%7D&CallbackMethod=PostObjectSearch&CallbackParmCount=1&__WWEVENTCALLBACK=&";
+            while (true)
+            {
+                Thread.Sleep(2000);
+                if (req.httpRequest("https://marknad.sgs.se/API/Service/SearchServiceHandler.ashx", info, Encoding.UTF8, Encoding.UTF8))
+                {
+                    break;
+                }
+            }
+            // Requires Microsoft.Toolkit.Uwp.Notifications NuGet package version 7.0 or greater
+            new ToastContentBuilder()
+                .AddArgument("action", "viewConversation")
+                .AddArgument("conversationId", 9813)
+                .AddText("SGS Release")
+                .Show(); // Not seeing the Show() method? Make sure you have version 7.0, and if you're using .NET 6 (or later), then your TFM must be net6.0-windows10.0.17763.0 or greater
+        }
+    }
+}
---
+++ Monitor-SGS/Request.cs
@@ -0,0 +1,79 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.IO;
+using System.IO.Compression;
+using System.Net;
+using System.Text;
+
+namespace Monitor_SGS
+{
+    internal class Request
+    {
+        public bool httpRequest(string url, string info, Encoding encoding, Encoding streamEncoding)
+        {
+        A: HttpWebRequest req = (HttpWebRequest)WebRequest.Create(url);
+            byte[] contentByte = streamEncoding.GetBytes(info);
+            req.Timeout = 5000;
+            req.Method = "POST";
+            req.ContentLength = contentByte.Length;
+            req.Accept = "*/*";
+            req.Headers.Add("Origin", "https://marknad.sgs.se");
+            req.Headers.Add("Sec-Fetch-Dest", "empty");
+            req.Headers.Add("Sec-Fetch-Mode", "cors");
+            req.Headers.Add("Sec-Fetch-Site", "same-origin");
+            req.Headers.Add("X-Requested-With", "XMLHttpRequest");
+            req.UserAgent = "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.67 Safari/537.36 OPR/87.0.4390.45";
+            req.ContentType = "application/x-www-form-urlencoded";
+            Stream webstream = req.GetRequestStream();
+            webstream.Write(contentByte, 0, contentByte.Length);
+            webstream.Close();
+            try
+            {
+                using (HttpWebResponse response = (HttpWebResponse)req.GetResponse())
+                {
+                    StreamReader readhtmlStream = null;
+                    string readtoend = null;
+                    if (response.ContentEncoding == "gzip")
+                    {
+                        Stream tokenStream = response.GetResponseStream();
+                        readhtmlStream = new StreamReader(new GZipStream(tokenStream, CompressionMode.Decompress), encoding);
+                        readtoend = readhtmlStream.ReadToEnd();
+                    }
+                    else
+                    {
+                        Stream tokenStream = response.GetResponseStream();
+                        readhtmlStream = new StreamReader(tokenStream, encoding);
+                        readtoend = readhtmlStream.ReadToEnd();
+                    }
+                    JObject jo = JObject.Parse(readtoend);
+                    try
+                    {
+                        if (int.Parse(jo["TotalCount"].ToString()) > 0)
+                        {
+                            Console.WriteLine("Release!!!!");
+                            return true;
+                        }
+                        Console.WriteLine("SGS Still Not Release");
+                        return false;
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Json Error");
+                        goto A;
+                    }
+                }
+
+            }
+            catch (WebException)
+            {
+                Console.WriteLine("Web Error");
+                goto A;
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Error");
+                goto A;
+            }
+        }
+    }
+}
